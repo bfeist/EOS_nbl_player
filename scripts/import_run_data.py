85c87abbf1ce7f99a63ac3aac46b1af55bb59769
Ben Feist <bf@benfeist.com>
2020-02-04 13:57:58 -0500

    Partially complete. Looping video feeds

--- scripts/import_run_data.py
+++ scripts/import_run_data.py
@@ -1,4 +1,4 @@
-import os, json, shutil
+import os, json, shutil, fnmatch, re
 
 runDataPath = '../_website/_webroot/run_data'
 
@@ -17,8 +17,60 @@ masterJSONDataFile.close()
 
 #---------------
 # process each run data folder
-for run_name in runList:
+for runName in runList:
+    runProcessedPath = runDataPath + '/' + runName + '/_processed'
     #create a fresh _processed folder in this run data folder
-    if os.path.isdir(runDataPath + '/' + run_name + '/_processed'):
-        shutil.rmtree(runDataPath + '/' + run_name + '/_processed')
-    os.mkdir(runDataPath + '/' + run_name + '/_processed')
+    # if os.path.isdir(runProcessedPath):
+    #     shutil.rmtree(runProcessedPath)
+    os.makedirs(runProcessedPath, exist_ok=True)
+
+    #get video stream names from video file names of video first segments (000 in filename)
+    videoStreamList = []
+    runVideoFeedsPath = runDataPath + '/' + runName + '/video_feeds'
+    lastStreamPrefix = ''
+    for streamFilename in os.listdir(runVideoFeedsPath):
+        if fnmatch.fnmatch(streamFilename, '*000.mp4'):
+            filename_root = streamFilename[:-8]
+
+            #get video start time
+            match = re.search(r'\[(.*)_(.*)\]', filename_root)
+            if match is not None:
+                dateStr = match.group(1)
+                timeStr = match.group(2).replace('-', ':')
+                isoStartTime = dateStr + "T" + timeStr + '-06:00'
+            else:
+                raise NameError('Failed to find valid timestamp in video stream filename')
+
+            #get crew sirname
+            match = re.search(r'.*?-(.*?)-\[.*', filename_root)
+            if match is not None:
+                crewSirname = match.group(1).capitalize()
+            else:
+                raise NameError('Failed to parse sirname')
+
+            #parse number of video segments
+            maxDigitVal = 0
+            for file in os.listdir(runVideoFeedsPath):
+                pattern = '*' + crewSirname.upper() + '*.mp4'
+                if fnmatch.fnmatch(file, pattern):
+                    match = re.search(r'.*\-(\d\d\d).mp4', file)
+                    if match is not None:
+                        digit = int(match.group(1))
+                        if digit > maxDigitVal:
+                            maxDigitVal = digit
+            if maxDigitVal == 0:
+                raise NameError('Something went wrong counting video segments')
+            else:
+                numberOfVideoSegments = maxDigitVal + 1
+
+            #assemble Dict object
+            streamDict = {}
+
+            streamDict['filename_root'] = filename_root
+            streamDict['start_time'] = isoStartTime
+            streamDict['number_of_segments'] = numberOfVideoSegments
+
+            videoStreamList.append(streamDict.copy())
+
+print ('done')
+# print (videoStreamList)
