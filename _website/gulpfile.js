c8beb58d86191ed60ebbb92632f5de5ae05cb8b6
Ben Feist <bf@benfeist.com>
2020-02-16 13:26:08 -0500

    gulp edits

--- _website/gulpfile.js
+++ _website/gulpfile.js
@@ -63,7 +63,6 @@ gulp.task('html', function () {
 
 gulp.task('browser-sync', function () {
     browserSync.init({
-        browser: "chrome",
         injectChanges: true,
         port: 3001,
         open: true,
